f8d4388681170ad9a72e23c5ce9f3440d1fa0e65
王文朗 <wangwenlang@gdhqx.com>
2025-03-19 18:02:25 +0800

    添加 论文docx和ppt

--- aes/test.py
+++ aes/test.py
@@ -1,14 +1,18 @@
 import pandas as pd
 from pandasql import sqldf
 
-df = pd.read_csv(r"D:\gs\distance_analysis\aes\out\second_clean\test\aes_g_c_v_2clean_full_vgc.csv")
+# df = pd.read_csv(r"D:\gs\distance_analysis\aes\out\second_clean\test\aes_g_c_v_2clean_full_vgc.csv")
+#
+# query = """select * from df where abs(score-Vocabulary) < 2 and abs(score-Grammar) < 2 and abs(score-Cohesion) < 2"""
+#
+# res = sqldf(query, locals())
+#
+# res.to_csv(r"D:\gs\distance_analysis\aes\out\second_clean\test\aes_vgc_3clean_full2.csv", index=False)
+#
+# print(res)
+#
+# print(res['score'].value_counts())
 
-query = """select * from df where abs(score-Vocabulary) < 2 and abs(score-Grammar) < 2 and abs(score-Cohesion) < 2"""
+df = pd.read_csv(r"D:\gs\distance_analysis\aes\dataset\ELLIPSE-Corpus\dl_full.csv")
 
-res = sqldf(query, locals())
-
-res.to_csv(r"D:\gs\distance_analysis\aes\out\second_clean\test\aes_vgc_3clean_full2.csv", index=False)
-
-print(res)
-
-print(res['score'].value_counts())
+print(df)
