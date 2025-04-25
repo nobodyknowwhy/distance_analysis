b67ae55f8a297636eb6582c13f29a7041cd100d5
王文朗 <wangwenlang@gdhqx.com>
2025-04-25 10:25:33 +0800

    我嘞个

--- leetcode/30.py
+++ leetcode/30.py
@@ -1,4 +1,5 @@
 def find_sub_str(s, words):
+    from collections import Counter
     for word in words:
         if not word in s:
             return []
@@ -9,13 +10,25 @@ def find_sub_str(s, words):
 
     len_words = len(words)
 
+    out_list = []
+
     if len(s) < len_windows:
         return []
 
     for i in range(0, len(s) - len_windows + 1):
+        dict_tar = {}
         words_windows = [s[i + x * len_word:i + (x + 1) * len_word] for x in range(len_words)]
 
-        print(words_windows)
+        for x in words_windows:
+            if x in s and x in dict_tar:
+                dict_tar[x] += 1
+            else:
+                dict_tar[x] = 1
+
+        if dict(Counter(words)) == dict_tar:
+            out_list.append(i)
+
+    return out_list
 
 
-find_sub_str("barfoothefoobarman", ["foo","bar"])
+print(find_sub_str("barfoothefoobarman", ["foo","bar"]))
